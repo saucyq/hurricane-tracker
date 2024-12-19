b9171bee025b4f3b5afc6a02dc33642755b58043
Cyril Telley <cyril.telley@gmail.com>
2024-12-19 13:27:43 +0100

    update plot

--- dashboard/pages/graphs.py
+++ dashboard/pages/graphs.py
@@ -3,13 +3,24 @@ from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
 import numpy as np
 import dash
 
 # Load your data
-df = pd.read_csv('C:/HES-SO/MA-VI/hurricane-tracker/data/AL.csv')
+df = pd.read_csv('data/AL.csv')
 df['year'] = pd.to_datetime(df['DateTime']).dt.year
+
+# Supposons que tu as un fichier CSV pour df_global
+df_global = pd.read_csv('data/GlobalTemperatures.csv')  # Remplace 'data/GlobalTemperatures.csv' par le chemin de ton fichier
+df_global['year'] = pd.to_datetime(df_global['dt']).dt.year
+
+# Supposons que tu as les fichiers CSV pour df_AL et df_EP
+df_AL = pd.read_csv('data/AL.csv')
+df_EP = pd.read_csv('data/EP.csv')
+df_AL['year'] = pd.to_datetime(df_AL['DateTime']).dt.year
+df_EP['year'] = pd.to_datetime(df_EP['DateTime']).dt.year
 
 dash.register_page(__name__, path='/graphs')
 
@@ -22,12 +33,17 @@ def update_cases_by_year_bar(id):
 
     fig = go.Figure()
     fig.add_trace(go.Bar(x=count['year'], y=count['count'], name='Number of cases by year'))
-    fig.update_layout(title='Number of cases by year', xaxis_title='Year', yaxis_title='Number of cases')
+    fig.update_layout(
+        title='Number of cases by year',
+        xaxis_title='Year',
+        yaxis_title='Number of cases',
+        height=450  # Hauteur fixe
+    )
 
     # Add rolling mean
     count['rolling_mean'] = count['count'].rolling(window=10).mean()
     fig.add_trace(go.Scatter(x=count['year'], y=count['rolling_mean'], mode='lines', name='Rolling mean of cases (10 years window)'))
-    fig.update_layout(legend=dict(x=0.35, y=-0.75, orientation='h'))
+    fig.update_layout(legend=dict(x=0.05, y=-0.25, orientation='h'))
 
     # Time series range slider
     fig.update_xaxes(rangeslider_visible=True)
@@ -55,56 +71,142 @@ def update_wind_speed_by_year(id):
     df_grouped_by_year_wind_min['rolling_mean_min'] = df_grouped_by_year_wind_min['min_wind'].rolling(window=10).mean()
     fig.add_trace(go.Scatter(x=df_grouped_by_year_wind_min['year'], y=df_grouped_by_year_wind_min['rolling_mean_min'], mode='lines', name='Rolling mean of min wind speed (10 years window)', line=dict(color='orange')))
 
-    fig.update_layout(legend=dict(x=0.4, y=-0.75, orientation='h'))
-    fig.update_layout(title='Wind speed by year')
-    fig.update_xaxes(title_text='Year')
-    fig.update_yaxes(title_text='Wind speed (mph)')
+    fig.update_layout(
+        legend=dict(x=0.05, y=-0.25, orientation='h'),
+        title='Wind speed by year',
+        xaxis_title='Year',
+        yaxis_title='Wind speed (mph)',
+        height=450  # Hauteur fixe
+    )
 
     fig.update_xaxes(rangeslider_visible=True)
     return fig
 
-
-# Callback to update the 'lat-lon-scatter' graph
+# Callback pour mettre à jour le graphique de corrélation
 @dash.callback(
-    Output('lat-lon-scatter', 'figure'),
-    Input('lat-lon-scatter', 'id'))
-def update_lat_lon_scatter(id):
-    fig = px.scatter(df, x="Longitude", y="Latitude", color="Wind", title='Hurricane path by wind speed')
-    fig.update_layout(legend=dict(x=0.75, y=-0.2, orientation='h'))
+    Output('correlation-graph', 'figure'),
+    Input('correlation-graph', 'id'))
+def update_correlation_graph(id):
+    df_global_grouped = df_global.groupby('year')['LandAverageTemperature'].mean().reset_index(name='mean_temp')
+    df_global_grouped['rolling_mean_temp'] = df_global_grouped['mean_temp'].rolling(window=10).mean()
+
+    count = df.groupby('year').size().reset_index(name='count')
+    count['rolling_mean'] = count['count'].rolling(window=10).mean()
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=df_global_grouped['year'], y=df_global_grouped['rolling_mean_temp'], mode='lines', name='Trend of temperature',))
+
+    fig.add_trace(go.Scatter(x=count['year'], y=count['rolling_mean'], mode='lines', name='Trend of cases', yaxis='y2', line=dict(color='red')))
+
+    fig.update_layout(
+        title='Correlation of the Trend Temperature and Trend of Cases by Year',
+        xaxis=dict(title='Year'),
+        yaxis=dict(
+            title='Temperature (°C)',
+            range=[df_global_grouped['mean_temp'].min(), df_global_grouped['mean_temp'].max()]
+        ),
+        yaxis2=dict(
+            title='Number of Cases',
+            overlaying='y',
+            side='right'
+        ),
+        legend=dict(x=0.05, y=-0.25, orientation='h'),
+        height=450  # Hauteur fixe
+    )
+
+    fig.update_xaxes(rangeslider_visible=True)
+    fig.update_xaxes(range=[1959, count['year'].max()])
     return fig
 
-# Callback to update the 'cases-per-storm-hist' graph
+# Callback pour mettre à jour le graphique du nombre de cas par an (AL et EP)
 @dash.callback(
-    Output('cases-per-storm-hist', 'figure'),
-    Input('cases-per-storm-hist', 'id'))
-def update_cases_per_storm_hist(id):
-    storm_counts = df.groupby('Storm')['DateTime'].count().reset_index(name='count')
-    fig = px.histogram(storm_counts, x="count", title='Number of measurements per storm', labels={'count': 'Number of measurements'})
-    fig.update_layout(legend=dict(x=0.75, y=-0.2, orientation='h'))
+    Output('cases-by-year-al-ep', 'figure'),
+    Input('cases-by-year-al-ep', 'id'))
+def update_cases_by_year_al_ep(id):
+    count_AL = df_AL.groupby('year').size().reset_index(name='count')
+    count_EP = df_EP.groupby('year').size().reset_index(name='count')
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=('Number of cases by year in AL', 'Number of cases by year in EP'))
+
+    fig.add_trace(go.Bar(x=count_AL['year'], y=count_AL['count'], name='Number of cases by year in AL'), row=1, col=1)
+    count_AL['rolling_mean'] = count_AL['count'].rolling(window=10).mean()
+    fig.add_trace(go.Scatter(x=count_AL['year'], y=count_AL['rolling_mean'], mode='lines', name='Trend of cases in AL'), row=1, col=1)
+
+    fig.add_trace(go.Bar(x=count_EP['year'], y=count_EP['count'], name='Number of cases by year in EP'), row=1, col=2)
+    count_EP['rolling_mean'] = count_EP['count'].rolling(window=10).mean()
+    fig.add_trace(go.Scatter(x=count_EP['year'], y=count_EP['rolling_mean'], mode='lines', name='Trend of cases in EP'), row=1, col=2)
+
+    fig.update_layout(
+        title_text='Number of cases by year in AL and EP',
+        xaxis_title='Year',
+        yaxis_title='Number of cases',
+        legend=dict(x=0.05, y=-0.25, orientation='h'),
+        height=450  # Hauteur fixe
+    )
+    return fig
+
+# Callback pour mettre à jour le graphique des tendances
+@dash.callback(
+    Output('trends-graph', 'figure'),
+    Input('trends-graph', 'id'))
+def update_trends_graph(id):
+    count = df.groupby('year').size().reset_index(name='count')
+    count['rolling_mean'] = count['count'].rolling(window=10).mean()
+    count_AL = df_AL.groupby('year').size().reset_index(name='count')
+    count_AL['rolling_mean'] = count_AL['count'].rolling(window=10).mean()
+    count_EP = df_EP.groupby('year').size().reset_index(name='count')
+    count_EP['rolling_mean'] = count_EP['count'].rolling(window=10).mean()
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=count['year'], y=count['rolling_mean'], mode='lines', name='Trend of cases'))
+    fig.add_trace(go.Scatter(x=count_AL['year'], y=count_AL['rolling_mean'], mode='lines', name='Trend of cases in AL'))
+    fig.add_trace(go.Scatter(x=count_EP['year'], y=count_EP['rolling_mean'], mode='lines', name='Trend of cases in EP'))
+    fig.update_layout(
+        title='Trends of cases by year',
+        xaxis_title='Year',
+        yaxis_title='Number of cases',
+        legend=dict(x=0.05, y=-0.25, orientation='h'),
+        height=450  # Hauteur fixe
+    )
     return fig
 
 # Layout of the app
 layout = html.Div(children=[
-    html.H3(children="Accident Analysis Dashboard"),
+    html.H3(children="Accident Analysis Dashboard", style={'textAlign': 'center'}),
 
     html.Div(children=[
         html.P(children="This dashboard provides an overview of the accidents data."),
         html.P(children="You can navigate through the different pages using the navigation bar."),
-    ], className='Info'),
+    ], className='Info', style={'textAlign': 'center'}),
 
-    # First row of graphs
+    # Première ligne avec deux graphiques
     html.Div(children=[
+        # Premier graphique
         dcc.Graph(id='cases-by-year-bar'),
+
+        # Deuxième graphique
         dcc.Graph(id='wind-speed-by-year'),
-    ], className='row'),
+    ], style={'display': 'flex', 'width': '100%'}),
 
+    # Graphiques suivants (un par ligne)
+    html.Div(children=[
+        dcc.Graph(id='correlation-graph'),
+    ], style={'width': '100%', 'padding': '0', 'margin': '0'}),
 
+    html.Div(children=[
+        dcc.Graph(id='cases-by-year-al-ep'),
+    ], style={'width': '100%', 'padding': '0', 'margin': '0'}),
+
+    html.Div(children=[
+        dcc.Graph(id='trends-graph'),
+    ], style={'width': '100%', 'padding': '0', 'margin': '0'}),
 
     html.Footer(className='home-footer', children=[
         html.Div(className='separator'),
         html.Div(className='container', children=[
             html.P('© 2024 - HES-SO Master. All rights reserved.', className='text-center'),
-            html.Div(className='text-center', children=[                
+            html.Div(className='text-center', children=[
                 html.Img(src='/assets/images/HES_SO_Logo.png', className='hesso-logo-footer'),
             ]),
             html.P('MA-VI Project - Telley Cyril / Saucy Quentin / Altin Hajda', className='text-center'),
--- dashboard/pages/map.py
+++ dashboard/pages/map.py
@@ -7,20 +7,17 @@ import plotly.graph_objects as go
 
 dash.register_page(__name__, path='/maps')
 
-# Read the data (adjust the path if needed)
-df = pd.read_csv('C:/HES-SO/MA-VI/hurricane-tracker/data/AL.csv')
+# Fusion des DataFrames
+dfDevastatorAt = pd.read_csv("data/AL.csv")
+dfDevastatorEP = pd.read_csv("data/EP.csv")
+df = pd.concat([dfDevastatorAt, dfDevastatorEP])
 df['DateTime'] = pd.to_datetime(df['DateTime'], errors='coerce').dt.tz_localize(None)
 df = df[df['DateTime'] >= '1950-01-01']
 
-# First point of the hurricane
+# Points de départ des ouragans
 set_df = df.drop_duplicates(subset='Key')
-ep = pd.read_csv('C:/HES-SO/MA-VI/hurricane-tracker/data/EP.csv')  # Adjust the path if needed
-ep['DateTime'] = pd.to_datetime(ep['DateTime'], errors='coerce').dt.tz_localize(None)
-ep = ep[ep['DateTime'] >= '1950-01-01']
-# First point of the hurricane
-set_ep = ep.drop_duplicates(subset='Key')
 
-# Create a basic map of the hurricane starting points using Plotly
+# Création de la carte initiale
 fig = go.Figure()
 
 fig.add_trace(go.Scattermapbox(
@@ -36,10 +33,9 @@ fig.add_trace(go.Scattermapbox(
             'Pressure: ' + set_df['Pressure'].astype(str)
     ),
     hoverinfo='text',
-    customdata=set_df[['Key', 'DateTime', 'Wind', 'Pressure']].values,  # Pass custom data for hover
+    customdata=set_df[['Key', 'DateTime', 'Wind', 'Pressure']].values,
 ))
 
-# Update layout
 fig.update_layout(
     title='Hurricane Starting Points',
     mapbox_style='carto-positron',
@@ -51,90 +47,115 @@ begin_date = set_df['DateTime'].dt.year.iloc[0]
 end_date = set_df['DateTime'].dt.year.iloc[-1]
 
 layout = html.Div(children=[
-    dcc.RangeSlider(begin_date, end_date, step=1,
-                    marks={str(year): str(year) for year in range(begin_date, end_date + 1, 10)},
-                    value=[begin_date, end_date],
-                    allowCross=False,
-                    id="date_select",
-                    tooltip={"placement": "bottom", "always_visible": True}),
+    html.H3('Maps', className='text-center mt-3'),
+
+    html.Div(className='Input', children=[
+        html.P('This page displays the starting points of hurricanes on a map.'),
+        html.P('You can select a date range to display the hurricanes that started in that range.'),
+        html.P('Click on a hurricane to see its path.'),
+        html.P('Click on the "Clear Selection" button to clear the selected hurricane path.'),
+    ]),
+
+    # Date range slider
+    html.Div(className='RadioButton mt-3', children=[
+        html.Label('Select Date Range:', className="label"),
+        dcc.RangeSlider(begin_date, end_date, step=1,
+                        marks={str(year): str(year) for year in range(begin_date, end_date + 1, 10)},
+                        value=[begin_date, end_date], allowCross=False, id='date_select',
+                        tooltip={'placement': 'bottom', 'always_visible': True}),
+    ]),
 
     html.Button("Clear Selection", id="clear-btn", n_clicks=0),
-    dcc.Graph(
-        id='map',
-        figure=fig,
-        config={'scrollZoom': True, 'displayModeBar': True}
-    ),
+    dcc.Graph(id='map', figure=fig, config={'scrollZoom': True, 'displayModeBar': True}),
     html.Div(id='output-container-date-picker-range'),
 
-    dcc.Link('Go back to home', href='/')
+    dcc.Link('Go back to home', href='/'),
+
+    html.Footer(className='home-footer', children=[
+        html.Div(className='separator'),
+        html.Div(className='container', children=[
+            html.P('© 2024 - HES-SO Master. All rights reserved.', className='text-center'),
+            html.Div(className='text-center', children=[
+                html.Img(src='/assets/images/HES_SO_Logo.png', className='hesso-logo-footer'),
+            ]),
+            html.P('MA-VI Project - Telley Cyril / Saucy Quentin / Altin Hajda', className='text-center'),
+        ]),
+    ]),
 ])
 
-# Callback to update the map based on the date range slider
+
+# Callback pour mettre à jour la carte en fonction de la plage de dates
 @dash.callback(
     Output('map', 'figure', allow_duplicate=True),
-    Input('date_select', 'value'),
+    [Input('date_select', 'value'),
+     Input('map', 'relayoutData')],
     prevent_initial_call=True)
-def update_output(value):
-    # Convert the selected range into date objects
+def update_output(value, relayoutData):
     start_date = pd.to_datetime(f"{value[0]}-01-01")
     end_date = pd.to_datetime(f"{value[1]}-12-31")
 
-    # Filter the data based on the selected date range
-    chosen_value = set_df[(set_df['DateTime'] >= start_date) & (set_df['DateTime'] <= end_date)]
+    if relayoutData is not None and 'mapbox.center' in relayoutData:
+        map_center = relayoutData['mapbox.center']
+        map_zoom = relayoutData['mapbox.zoom']
+    else:
+        map_center = {'lat': 20, 'lon': -60}
+        map_zoom = 3
 
+    chosen_value = set_df[(set_df['DateTime'] >= start_date) & (set_df['DateTime'] <= end_date)]
     updated_fig = drawmap(chosen_value)
-    # Update layout, including the title
     updated_fig.update_layout(
         title=f'Hurricane Starting Points: {value[0]} to {value[1]}',
         mapbox_style='carto-positron',
-        mapbox=dict(center={'lat': 20, 'lon': -60}, zoom=3),
+        mapbox=dict(center=map_center, zoom=map_zoom),
         height=900
     )
     return updated_fig
 
 
-# Callback to display hurricane path on click
+# Callback pour afficher le chemin de l'ouragan au clic
 @dash.callback(
     Output('map', 'figure', allow_duplicate=True),
     [Input('map', 'clickData'),
-     Input('date_select', 'value')],
+     Input('date_select', 'value'),
+     Input('map', 'relayoutData')],
     prevent_initial_call=True
 )
-def display_path_on_click(clickData, value):
-    # Default figure
-    path_fig = go.Figure(fig)
+def display_path_on_click(clickData, value, relayoutData):
     if clickData is None:
         return dash.no_update
-    
-    # Get the selected hurricane's Key from the clickData
-    selected_key = clickData['points'][0]['customdata'][0] 
+
+    selected_key = clickData['points'][0]['customdata'][0]
+
+    if relayoutData is not None and 'mapbox.center' in relayoutData:
+        map_center = relayoutData['mapbox.center']
+        map_zoom = relayoutData['mapbox.zoom']
+    else:
+        map_center = {'lat': 20, 'lon': -60}
+        map_zoom = 3
 
     selected_key = str(selected_key).strip()
     df['Key'] = df['Key'].astype(str).str.strip()
-    chosen_value = set_df[str(value[0]) + '-01-01' <= set_df['DateTime']]
-    chosen_value = chosen_value[chosen_value['DateTime'] <= str(value[1])]
+    chosen_value = set_df[(set_df['DateTime'] >= pd.to_datetime(f"{value[0]}-01-01")) & (set_df['DateTime'] <= pd.to_datetime(f"{value[1]}-12-31"))]
     hurricane_details = df[df['Key'] == selected_key]
 
-    # Normalize date to get a linear color diff
     min_time = hurricane_details['DateTime'].min()
     max_time = hurricane_details['DateTime'].max()
     hurricane_details['time_normalized'] = (hurricane_details['DateTime'] - min_time) / (max_time - min_time)
     path_fig = drawmap(chosen_value)
-    
-    # Draw line, purple with a marker for the beginning and one for the end
-    path_fig.add_trace(go.Scattermapbox(
-        mode='lines',
-        lon=hurricane_details['Lon'],
-        lat=hurricane_details['Lat'],
-        line=dict(
-            width=4,
-            color='purple',
-        ),
-        hoverinfo='text',
-        customdata=hurricane_details['Key'],
-        text=hurricane_details['Name'],
-        showlegend=False  # No legend for individual segments
-    ))
+
+    for i in range(len(hurricane_details) - 1):
+        path_fig.add_trace(go.Scattermapbox(
+            mode='lines',
+            lon=hurricane_details['Lon'].iloc[i:i + 2],
+            lat=hurricane_details['Lat'].iloc[i:i + 2],
+            line=dict(
+                width=4,
+                color='red' if hurricane_details['Status'].iloc[i] == 'HU' else 'blue'
+            ),
+            hoverinfo='text',
+            text=hurricane_details['Name'].iloc[i],
+            showlegend=False
+        ))
 
     start_point = hurricane_details.iloc[0]
     end_point = hurricane_details.iloc[-1]
@@ -152,57 +173,57 @@ def display_path_on_click(clickData, value):
         hoverinfo='text',
         showlegend=False
     ))
+
     path_fig.update_layout(
         mapbox_style='carto-positron',
         title=f'Hurricane Path: {selected_key}',
         height=900,
-        mapbox=dict(
-            center={'lat': 20, 'lon': -60},  # Fixed center for the map
-            zoom=3  # Fixed zoom level
-        ),
+        mapbox=dict(center=map_center, zoom=map_zoom),
         coloraxis_showscale=False
     )
-    path_fig.update_layout(mapbox_style='carto-positron')
 
     return path_fig
 
-# Callback to clear click data
+
+# Callback pour effacer les données de clic
 @dash.callback(
-    [Output('map', 'clickData'),Output('map', 'figure', allow_duplicate=True)],
+    [Output('map', 'clickData'), Output('map', 'figure', allow_duplicate=True)],
     [Input('clear-btn', 'n_clicks'),
      Input('date_select', 'value')],
     prevent_initial_call=True
 )
-def clear_click_data(n_clicks,value):
+def clear_click_data(n_clicks, value):
     if n_clicks > 0:
-        chosen_value = set_df[str(value[0]) + '-01-01' <= set_df['DateTime']]
-        chosen_value = chosen_value[chosen_value['DateTime'] <= str(value[1])]
-        to_ret =drawmap(chosen_value)
+        chosen_value = set_df[(set_df['DateTime'] >= pd.to_datetime(f"{value[0]}-01-01")) & (
+                    set_df['DateTime'] <= pd.to_datetime(f"{value[1]}-12-31"))]
+        to_ret = drawmap(chosen_value)
         to_ret.update_layout(
             mapbox_style='carto-positron',
             title=f'Hurricane Path',
             height=900,
             mapbox=dict(
-                center={'lat': 20, 'lon': -60},  # Fixed center for the map
-                zoom=3  # Fixed zoom level
+                center={'lat': 20, 'lon': -60},
+                zoom=3
             ),
             coloraxis_showscale=False
         )
-        return None,to_ret  # Reset clickData to None when the button is clicked
-    return dash.no_update,dash.no_update  # Otherwise, do nothing
+        return None, to_ret
+    return dash.no_update, dash.no_update
 
+
+# Fonction pour dessiner la carte
 def drawmap(dfDraw):
     updated_fig = go.Figure()
-
-    # Clear any previous data from the figure
     updated_fig.data = []
 
-    # Add the new data as a trace
+    dfDraw['Color'] = dfDraw['Status'].apply(
+        lambda x: 'red' if x == 'HU' else 'green')
+
     updated_fig.add_trace(go.Scattermapbox(
         mode='markers',
         lon=dfDraw['Lon'],
         lat=dfDraw['Lat'],
-        marker=dict(size=7, color='blue'),
+        marker=dict(size=7, color=dfDraw['Color']),
         text=(
                 'Name: ' + dfDraw['Name'] + '<br>' +
                 'Key: ' + dfDraw['Key'] + '<br>' +
@@ -210,40 +231,8 @@ def drawmap(dfDraw):
                 'Wind: ' + dfDraw['Wind'].astype(str) + '<br>' +
                 'Pressure: ' + dfDraw['Pressure'].astype(str)
         ),
-        hoverinfo='text',  # Display custom hover text,
-
+        hoverinfo='text',
         customdata=dfDraw[['Key', 'DateTime', 'Wind', 'Pressure']].values
     ))
 
     return updated_fig
-
-layout = html.Div(children=[
-    html.H3('Maps', className='text-center mt-3'),
-    
-    html.Div(className='Input', children=[
-        html.P('This page displays the starting points of hurricanes on a map.'),
-        html.P('You can select a date range to display the hurricanes that started in that range.'),
-        html.P('Click on a hurricane to see its path.'),
-        html.P('Click on the "Clear Selection" button to clear the selected hurricane path.'),
-    ]),
-    
-    # Date range slider
-    html.Div(className='RadioButton mt-3', children=[
-        html.Label('Select Date Range:', className="label"),
-        dcc.RangeSlider(begin_date, end_date, step=1, marks={str(year): str(year) for year in range(begin_date, end_date + 1, 10)}, value=[begin_date, end_date], allowCross=False, id='date_select', tooltip={'placement': 'bottom', 'always_visible': True}),
-    ]),    
-    
-    dcc.Graph(id='map', figure=fig, config={'scrollZoom': True, 'displayModeBar': True}),        
-    
-    
-    html.Footer(className='home-footer', children=[
-        html.Div(className='separator'),
-        html.Div(className='container', children=[
-            html.P('© 2024 - HES-SO Master. All rights reserved.', className='text-center'),
-            html.Div(className='text-center', children=[                
-                html.Img(src='/assets/images/HES_SO_Logo.png', className='hesso-logo-footer'),
-            ]),
-            html.P('MA-VI Project - Telley Cyril / Saucy Quentin / Altin Hajda', className='text-center'),
-        ]),
-    ]),
-])
